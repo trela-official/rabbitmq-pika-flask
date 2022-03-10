8acfd416ecd8e98aaa85ec37ce473ce0db772ef8
Pedro Azambuja <pedro.azambuja@zapt.com.br>
2022-03-10 14:00:22 -0300

    Fixes Issue #19

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='rabbitmq_pika_flask',
     packages=['rabbitmq_pika_flask'],   # Chose the same as "name"
     # Start with a small number and increase it with every change you make
-    version='1.2.14',
+    version='1.2.15',
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
