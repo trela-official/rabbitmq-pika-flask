46af052835c6bc51aad18c7efd7d6d8b8d862fb8
Rafael Perrella <rafael.perrella@trela.com.br>
2022-12-13 10:10:26 -0300

    Re-export RabbitConsumerMessage and others

--- rabbitmq_pika_flask/__init__.py
+++ rabbitmq_pika_flask/__init__.py
@@ -1,2 +1,18 @@
 from .RabbitMQ import ExchangeType
 from .RabbitMQ import RabbitMQ
+from .RabbitConsumerMessage import (
+    RabbitConsumerMessage,
+    RabbitConsumerMiddleware,
+    RabbitConsumerMiddlewareCallNext,
+    RabbitConsumerMiddlewareError,
+)
+
+
+__all__ = [
+    "ExchangeType",
+    "RabbitMQ",
+    "RabbitConsumerMessage",
+    "RabbitConsumerMiddleware",
+    "RabbitConsumerMiddlewareCallNext",
+    "RabbitConsumerMiddlewareError",
+]
